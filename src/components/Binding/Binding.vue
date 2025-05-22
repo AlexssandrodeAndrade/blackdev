dcf80a2548b0fd5985afb0883c96d73d65d17a45
Alexssandro de Andrade <leko.alexssandro@gmail.com>
2025-05-22 16:59:07 -0300

    22-05-2025 formulario tipos de campos

--- src/components/Binding/Binding.vue
+++ src/components/Binding/Binding.vue
@@ -1,14 +1,15 @@
 <template>
   <h1>Bindings</h1>
 
-  <input :type="input.type" :name="input.nameUser" />
-  <button @click="showHidePassword">Mostrar/Ocutar Senha</button>
+  <input :type="input.type" :id="input.id" :name="input.nomeUser" :placeholder="input.placeholder" />
+  <button @click.prevent="showHidePassword">Mostrar/Ocutar Senha</button>
 </template>
 <script setup>
 import { reactive } from 'vue'
 
 const input = reactive({
-  name: 'nameUser',
+  id: 'nomeUser',
+  name: 'nomeUser',
   type: 'password',
   placeholder: 'Digite um nome aqui',
 })
---
+++ src/components/LifeCycle/CompositionApi.vue
@@ -0,0 +1,40 @@
+<template>
+<h1>CompositionApi</h1>
+
+<p>O contador esta no número: {{ counter }}</p>
+<button @click="counter++">Somar</button>
+</template>
+
+<script setup>
+import {
+    onBeforeMount,
+    onBeforeUpdate,
+    onBeforeUnmount,
+    onMounted,
+    onUpdated,
+    onUnmounted,
+ref
+} from 'vue'
+
+  const counter = ref(0)
+
+  onBeforeMount(()=> {
+    console.log('1 Composition API - Before Mounted')
+  })
+  onMounted(()=> {
+    console.log('2 Composition API - Mounted')
+  })
+  onBeforeUpdate(()=> {
+    console.log('3 Composition API - Before Update')
+  })
+  onUpdated(()=> {
+    console.log('4 Composition API - updated')
+  })
+  onBeforeUnmount(()=> {
+    console.log('5 Composition API - Before Unmounted')
+  })
+  onUnmounted(()=> {
+    console.log('6 Composition API - Unmounted')
+  })
+
+</script>
