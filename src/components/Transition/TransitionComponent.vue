152613e231ee54287e9e224d88e50df0610767f4
Alexssandro de Andrade <leko.alexssandro@gmail.com>
2025-05-26 17:27:41 -0300

    commit

--- src/components/Transition/TransitionComponent.vue
+++ src/components/Transition/TransitionComponent.vue
@@ -1,8 +1,15 @@
-template>
-
-<div style="padding: 30px;">
-<p style="background: #999; padding: 30px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos laudantium magnam veniam ducimus aspernatur iste voluptatibus asperiores exercitationem reiciendis delectus nemo quidem sit, nisi dicta dolore ab nam architecto mollitia eaque dolorum eligendi velit recusandae minus. Illum expedita, velit beatae ipsam itaque aliquid adipisci nemo non maiores aperiam soluta repudiandae illo consequatur ducimus dolor natus rerum corrupti tempore. Sit ducimus a ratione veniam, voluptatum optio ab nam officiis, necessitatibus, obcaecati voluptates earum dolorem. Consequatur quae aperiam aliquam praesentium dolorem tenetur at laborum dignissimos et, facilis omnis repellendus est quisquam minima inventore nulla labore quasi eligendi officiis repudiandae? Rerum, sint repellendus!</p>
-</div>
-
-
+<template>
+  <div style="padding: 30px">
+    <p style="background: #999; padding: 30px">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos laudantium magnam veniam
+      ducimus aspernatur iste voluptatibus asperiores exercitationem reiciendis delectus nemo quidem
+      sit, nisi dicta dolore ab nam architecto mollitia eaque dolorum eligendi velit recusandae
+      minus. Illum expedita, velit beatae ipsam itaque aliquid adipisci nemo non maiores aperiam
+      soluta repudiandae illo consequatur ducimus dolor natus rerum corrupti tempore. Sit ducimus a
+      ratione veniam, voluptatum optio ab nam officiis, necessitatibus, obcaecati voluptates earum
+      dolorem. Consequatur quae aperiam aliquam praesentium dolorem tenetur at laborum dignissimos
+      et, facilis omnis repellendus est quisquam minima inventore nulla labore quasi eligendi
+      officiis repudiandae? Rerum, sint repellendus!
+    </p>
+  </div>
 </template>
